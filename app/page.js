c03fdde68eb7184b995300b3fcdb7c0cf7567422
Your Name <reannejanelle@outlook.com>
2026-01-07 18:35:20 +1000

    react implementing

---
+++ app/page.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Home() {
+    return (
+        <main>
+            <h1>Collect customer feedback to build better products</h1>
+            <div>
+            Create a feedback board in minutes, prioritise features, and 
+            build products your customers will love.
+            </div>
+            <Link href="/dashboard">Dashboard</Link>
+
+            <button onClick={() => console.log("Logging in...")}>Login</button>
+        </main>
+    );
+    }
